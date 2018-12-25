2aff6de3638406ee316b19570311e40343c9d87f
Vikrant Gajria <vikrantgajria@gmail.com>
2018-12-25 11:15:46 +0530

    Add prototypes for all models.

--- canteenWeb/models.py
+++ canteenWeb/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#########################
+# AUTHENTICATION SYSTEM #
+#########################
+
+class User(AbstractUser):
+    pass
+
+class StudentProfile(models.Model):
+    pass
+
+class TeacherProfile(models.Model):
+    pass
+
+#####################
+# CUSTOMIZABLE MENU #
+#####################
+
+class MenuItem(models.Model):
+    pass
+
+###################
+# ORDERING SYSTEM #
+###################
+
+class OrderItem(models.Model):
+    pass
+
+class Order(models.Model):
+    pass
+
+##################
+# BILLING SYSTEM #
+##################
+
+class Bill(models.Model):
+    pass
