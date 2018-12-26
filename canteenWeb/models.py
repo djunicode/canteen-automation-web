4ed588ab30c58ceecc95b8d2e3cd73c0b07270b8
preet4737 <preet4737@gmail.com>
2018-12-26 11:10:19 +0530

    All profiles added for auth

--- canteenWeb/models.py
+++ canteenWeb/models.py
@@ -7,15 +7,27 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
-
+    USERNAME_FIELD = models.CharField(max_length = 30,unique = True, default = "")  #sap id
+    EMAIL_FIELD = models.EmailField(max_length = 40, blank = True, unique=True)
+    first_name = models.CharField(max_length = 15)
+    last_name = models.CharField(max_length = 15)
+    phone_number = models.CharField(max_length = 12, blank = True)
+    device_id = models.CharField(max_length = 30, default = "")# For use by android team
+    is_student = models.BooleanField(default = True)# to check whether the user is student
+    is_teacher = models.BooleanField(default = False)# to check whether the user is teacher
+    is_banned = models.BooleanField(default = False) # To check whether the user is banned
 
 class StudentProfile(models.Model):
-    pass
+    student =  models.OneToOneField(User, on_delete = models.CASCADE, null = True)
+    division = models.CharField(max_length = 1, default = 'A')
+    department = models.CharField(max_length = 30, default = None)
+    admission_year = models.PositiveSmallIntegerField(default = 1)
 
 
 class TeacherProfile(models.Model):
-    pass
+    teacher =  models.OneToOneField(User, on_delete = models.CASCADE, null = True)
+    floor = models.PositiveSmallIntegerField(default = None)
+    department = models.CharField(max_length = 30, default = None)
 
 
 #####################
