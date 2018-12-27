1ce0c6b18290d28bc0060a08c2c3e37263064675
preet4737 <preet4737@gmail.com>
2018-12-27 11:25:50 +0530

    Some changes

--- canteenWeb/admin.py
+++ canteenWeb/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 from .models import User
+from django import forms
 
 # Our custom user model.
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('__all__')
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('phone_number', 'is_banned', 'is_student', 'is_teacher', 'device_id')
+
+class UserAdmin(BaseUserAdmin):
+    form = UserForm
+    add_form = UserUpdateForm
+
+    add_fieldsets = (
+    (None,{
+    'classes': ('wide',),
+    'fields': ('phone_number', 'is_banned', 'is_student', 'is_teacher', 'device_id')
+    }
+    ),
+    )
+
+
+
+
 admin.site.register(User, UserAdmin)
 
 # Remove groups from admin site.
