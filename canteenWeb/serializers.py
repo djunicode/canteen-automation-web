906fab27a5c63f6984b12db95864380056c0fafa
amoghparab247 <mumbai.amoghparab@gmail.com>
2018-12-29 12:59:41 +0530

    3 files reformatted

--- canteenWeb/serializers.py
+++ canteenWeb/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import *
+from .models import MenuItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
--- canteenWeb/views.py
+++ canteenWeb/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from .models import *
-from .serializers import *
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 # Create your views here.
-
 class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
