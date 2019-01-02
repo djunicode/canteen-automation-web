219cd86210492247dfdd2da0ff053e96bbaf5066
Chinmay Khamkar <khamkarchinmay4@gmail.com>
2019-01-03 01:09:43 +0530

    added navbar

--- canteenFrontend/canteen/src/App.js
+++ canteenFrontend/canteen/src/App.js
@@ -1,32 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import MenuAppBar from './components/MenuAppBar';
+import Footer from './components/Footer';
 import './App.css';
+import PermanentDrawerLeft from './components/PermanentDrawerLeft';
+import { Divider } from '@material-ui/core';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>THIS WILL BE FUN!</h1>
-          <h2>Canteen Web Team is much better than the other teams</h2>
-          <p>Also, Frontend team is much better than backend team</p>
-          <p>All the best Manav, Shreya, Vikrant, Chinmay! Happy coding :p</p>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function App() {
+
+  return(
+   
+      <div>
+        <MenuAppBar/>
+        <PermanentDrawerLeft/>
       </div>
-    );
-  }
+      
+  
+  )
 }
 
 export default App;
---
+++ canteenFrontend/canteen/src/components/MenuAppBar.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+    
+  },
+  
+};
+class MenuAppBar extends React.Component {
+    state = {
+      
+      anchorEl: null,
+    };
+  
+    handleChange = event => {
+      this.setState({ auth: event.target.checked });
+    };
+  
+    handleMenu = event => {
+      this.setState({ anchorEl: event.currentTarget });
+    };
+  
+    handleClose = () => {
+      this.setState({ anchorEl: null });
+    };
+  
+    render() {
+      const { classes } = this.props;
+      const {  anchorEl } = this.state;
+      const open = Boolean(anchorEl);
+  
+      return (
+        <div>
+        <div className={classes.root}>
+          
+          <AppBar position="fixed">
+            <Toolbar>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />{/* logo */}
+  
+              </IconButton>
+              <Typography variant="h6" align="right" color="inherit" className={classes.grow}>
+              john
+              </Typography>
+              
+                <div>
+                  <IconButton
+                    aria-owns={open ? 'menu-appbar' : undefined}
+                    aria-haspopup="true"
+                    onClick={this.handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle fontSize="large" />
+                  </IconButton>
+                  
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={this.handleClose}
+                  >                
+              
+                  <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                  
+                  </Menu>
+                
+                </div>
+              
+            </Toolbar>
+          </AppBar>
+        </div>
+        </div>
+      );
+    }
+  }
+
+  MenuAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+
+export default withStyles(styles)(MenuAppBar);
+
