8157beae08c83144306d6ac8f11e0b9960722c35
Chinmay Khamkar <khamkarchinmay4@gmail.com>
2019-01-03 16:08:37 +0530

    minor issues solved

--- canteenFrontend/canteen/src/App.js
+++ canteenFrontend/canteen/src/App.js
@@ -3,7 +3,7 @@ import MenuAppBar from './components/MenuAppBar';
 import Footer from './components/Footer';
 import './App.css';
 import PermanentDrawerLeft from './components/PermanentDrawerLeft';
-import { Divider } from '@material-ui/core';
+
 
 function App() {
 
