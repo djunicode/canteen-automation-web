6e254cfd338f36c1c69ecb95e38ae8ba21f7133a
preet4737 <preet4737@gmail.com>
2019-01-10 22:49:39 +0530

    modified Menu Items

--- canteenFrontend/canteen/src/components/NavHome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-const NavHome = () => {
-      return (  
-            <NavLink to="/">Home</NavLink>
-      );
-}
- 
-export default NavHome;
