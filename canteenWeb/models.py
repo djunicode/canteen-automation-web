cfbc9f66d183e96a94a194f2b6b1f1ddcd91c4e1
preet4737 <preet4737@gmail.com>
2019-01-11 08:06:21 +0530

    Increase max length of category

--- canteenWeb/models.py
+++ canteenWeb/models.py
@@ -49,7 +49,7 @@ class MenuItem(models.Model):
         max_length=8, choices=choices.MENU_ITEM_CHOICES, default="NON JAIN"
     )
     category = models.CharField(
-        max_length=20, choices=choices.MENU_ITEM_CATEGORY, default="FAST FOOD"
+        max_length=32, choices=choices.MENU_ITEM_CATEGORY, default="FAST FOOD"
     )
 
     def __str__(self):
