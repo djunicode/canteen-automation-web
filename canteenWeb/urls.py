e268a2d95ca8b06b32672b3959aa1881a89ea032
amoghparab247 <mumbai.amoghparab@gmail.com>
2019-01-17 22:57:31 +0530

    Restored back the original urls

--- canteenWeb/urls.py
+++ canteenWeb/urls.py
@@ -19,26 +19,10 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register("orders", views.OrderViewSet)
+router.register("menu", views.MenuViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path(
-        "menu_item/",
-        views.MenuViewSet.as_view({"get": "list", "post": "create"}),
-        name="menuitem-list",
-    ),
-    path(
-        "menu_item/<int:pk>/",
-        views.MenuViewSet.as_view(
-            {
-                "get": "retrieve",
-                "put": "update",
-                "patch": "partial_update",
-                "delete": "destroy",
-            }
-        ),
-        name="menuitem-detail",
-    ),
     path("signup/", views.SignUp.as_view(), name="signup"),
     path("login/", views.Login.as_view(), name="login"),
     path("logout/", views.Logout.as_view(), name="logout"),
