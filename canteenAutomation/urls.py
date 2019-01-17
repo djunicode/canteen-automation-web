4f6ce47d63ced18464c1f745204b2f2dcca21e51
Vikrant Gajria <vikrantgajria@gmail.com>
2019-01-17 21:51:49 +0530

    Add drf-yasg for api end-points documentation

--- canteenAutomation/urls.py
+++ canteenAutomation/urls.py
@@ -15,5 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("canteenWeb.urls"))]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Canteen API",
+        default_version="v1",
+        description="All canteen endpoints",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("canteenWeb.urls")),
+    path("doc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
