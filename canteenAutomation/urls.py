22408c39c06c0025ceb6e2508993b9d9a399ae67
Vikrant Gajria <vikrantgajria@gmail.com>
2019-01-17 22:31:31 +0530

    Add compressed Swagger view

--- canteenAutomation/urls.py
+++ canteenAutomation/urls.py
@@ -31,5 +31,12 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("canteenWeb.urls")),
-    path("doc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path(
+        "doc/",
+        schema_view.with_ui("swagger", cache_timeout=1000),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/", schema_view.with_ui("redoc", cache_timeout=1000), name="schema-redoc"
+    ),
 ]
