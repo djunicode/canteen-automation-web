2606c96255515512b9cdc6288bc4608aa4338219
Chinmay Khamkar <khamkarchinmay4@gmail.com>
2019-01-21 00:24:25 +0530

    pending orders card created

--- canteenFrontend/src/components/core/BarButton.js
+++ canteenFrontend/src/components/core/BarButton.js
@@ -1,7 +1,7 @@
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
-const styles = theme => ({
+const styles = (theme) => ({
     root: {
         color: theme.palette.primary.main,
         margin: theme.spacing.unit,
