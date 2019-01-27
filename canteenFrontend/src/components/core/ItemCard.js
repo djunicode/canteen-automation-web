0721dacdb1c0bbf434d5b6df1741e54cb2f14337
ma9shah <35427989+ma9shah@users.noreply.github.com>
2019-01-27 22:34:32 +0530

    Add files via upload

---
+++ canteenFrontend/src/components/core/ItemCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import styles from './Style_ItemsCard'
+
+import styles from './styles'
+import Typography from "@material-ui/core/Typography";
+import { Divider } from '@material-ui/core';
+function ItemCard(props) {
+    return (
+        <div>
+            <br />
+            <div style={styles.box1}>
+                <div style={styles.item1}>
+                    <br />
+                    <div style={styles.attribute3}>NAME <input style={styles.input} placeholder={props.Name}></input></div>
+                    <div style={styles.attribute1}>INGREDIENTS <input style={styles.input} placeholder={props.Ing}></input></div>
+                    <div style={styles.attribute4}>Price <input style={styles.input2} placeholder={props.Price}></input></div>
+                </div>
+                <a style={styles.icon_trash} className="fa fa-trash fa-2x" aria-hidden="true"></a>
+                <a style={styles.icon_tick} className="fa fa-check-circle fa-2x" aria-hidden="true"></a>
+            </div>
+        </div>
+    );
+}
+
+export default ItemCard;
