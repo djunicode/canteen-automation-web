3340451be4211e1e9b45e6c08c43d29ff2e9a921
preet4737 <preet4737@gmail.com>
2019-09-01 00:52:19 +0530

    Added Delete Mutation for Category and MenuItem

--- canteenGraphql/mutation.py
+++ canteenGraphql/mutation.py
@@ -1,7 +1,10 @@
 from .forms import *
+from .types import *
+from canteenDb.models import *
 from graphene_django.forms.mutation import DjangoModelFormMutation
 from graphene_django.rest_framework.mutation import SerializerMutation
-from graphene import ObjectType
+import graphene
+from graphene import ObjectType, Field, ID
 
 
 class OrderMutation(SerializerMutation):
@@ -24,8 +27,35 @@ class MenuItemMutation(DjangoModelFormMutation):
     class Meta:
         form_class = MenuItemForm
 
+class DeleteCategoryMutation(graphene.Mutation):
+    ok = graphene.Boolean()
+    class Arguments:
+        # The input arguments for this mutation
+        id = ID()
+
+    @classmethod
+    def mutate(cls, root, info, **args):
+        obj = Category.objects.get(pk=args["id"])
+        obj.delete()
+        return cls(ok=True)
+
+
+class DeleteMenuItemMutation(graphene.Mutation):
+    ok = graphene.Boolean()
+    class Arguments:
+        # The input arguments for this mutation
+        id = ID()
+
+    @classmethod
+    def mutate(cls, root, info, **args):
+        obj = MenuItem.objects.get(pk=args["id"])
+        obj.delete()
+        return cls(ok=True)
+
 
 class Mutation(ObjectType):
     order_mutation = OrderMutation.Field()
     category_mutation = CategoryMutation.Field()
     menu_item_mutation = MenuItemMutation.Field()
+    delete_category_mutation = DeleteCategoryMutation.Field()
+    delete_menu_item_mutation = DeleteMenuItemMutation.Field()
